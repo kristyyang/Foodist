10326b65126b8a4d3088b90b42b1cc428d72e430
Kaitian Xie <xkaitian@gmail.com>
2019-03-03 16:48:27 -0800

    Create a basic server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+const app = express();
+
+// Load middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set up mongoose connection
+const mongoURI = require("./config/keys").mongoURI;
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected!"))
+  .catch(error => console.log(error));
+
+// Start server
+const port = 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
