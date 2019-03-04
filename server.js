4ef077ede77b26dca26819577178b1fb4e68972f
Kaitian Xie <xkaitian@gmail.com>
2019-03-04 00:41:33 -0800

    Use morgan

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const passport = require('passport');
 const path = require('path');
 
@@ -11,6 +12,7 @@ const app = express();
 // Load middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 app.use(passport.initialize());
 
 // Set up mongoose connection
