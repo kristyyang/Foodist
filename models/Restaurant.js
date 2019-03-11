43d210bf79cca5fa24151ca4ee207a8829608bc7
Kaitian Xie <xkaitian@gmail.com>
2019-03-10 17:48:54 -0700

    resId is unique

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const RestaurantSchema = new Schema({
   resId: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   name: {
     type: String,
